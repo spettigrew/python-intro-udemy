ad47cd411f224ac9fe923ee6cbfbb145ba76a90e
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-09-18 13:43:47 -0600

    Notes added to intro to python file

---
+++ python problems/intro.py
@@ -0,0 +1,23 @@
+#this is the first comment - single line comment
+
+spam = 1 # and this is the second comment
+        # and now a third
+
+text = "# this is not a comment because it's inside quotes."
+
+
+#------------------------------------------------------
+
+#Numbers
+ #int - whole numbers, no decimal points
+ #float - numbers with decimal points
+ #complex - x + y = j x is real, y is imaginary.
+ # +, -, *, / and squaring. If it's not a number, it's an operation.
+
+ # Arithmetic order
+ # () - caluclate your parenthesis first
+ # Exponents - if they exist
+ # * and / - from left to right
+ # + and - - from left to right
+
+
