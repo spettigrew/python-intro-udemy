6cdb53942078394eb5021a3f6c8816ad77b1ced8
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-09-22 16:56:50 -0600

    Notes added to files

--- python problems/intro.py
+++ python problems/intro.py
@@ -20,5 +20,27 @@ text = "# this is not a comment because it's inside quotes."
  # * and / - from left to right
  # + and - - from left to right
 
-## Variable: to store values for processing. Python is a OOP (Object Oriented Programming). Every variable is an object.
-## variable = value
+## Variable: to store values for processing. Python is a OOP (Object Oriented Programming), interpreted language. Every variable is an object.
+## variable = value. Strings are immutable - can not change.
+#-----------------------------------------------------------
+
+## Lists - contain items of the same type. They are mutable. Start at the index of 0.
+# slice - remove from the end of the list. Append - add to the end of the list.
+# [:] = [] will clear a list
+
+## Conditional statements
+#if, if/else = elif. Inputs based on conditional statement. Elif writes another condition if the first if statement is false. Else catches what is not caught by the preceding conditions.
+# for loop: for iterator_var in sequeence:
+        #statement(s)
+# range[start,stop,step] can be positive or negative, but not 0.
+        #for i in range(5):
+        #       print(i) : 0-5 printed.
+
+        #for i in range(0, 10, 3)
+        #       print(i) : 0,3,6,9 printed. 3 is the rate of increase.
+
+        # a = ["I", "want", "to", "eat."]
+        # for i in range(len(a)):
+        #       print(i, a[i]) : 0-3 printed I, want, to eat.  
+
+# sum(range(4)) : prints 6, 0+1+2+3=6
--- python problems/main.py
+++ python problems/main.py
@@ -1,2 +1,21 @@
 #first app in python;
 print('Helloj World!')
+
+# x = int(input("Enter an integar: "))
+# enter an integar: 40
+
+# if x < 0:
+#     x = 0
+#     print('Negative change to zero')
+# elif x == 0:
+#    print('Zero')
+# elif x == 1:
+#   print("Single")
+
+## The above example is false.
+
+# else:
+#   print('More')
+
+## Now the above statement is true.
+
